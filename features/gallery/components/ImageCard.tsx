363ccbf04f55e14618fe4050569bf4ab1dc394af
Ryan-Oneil <44525616+Ryan-Oneil@users.noreply.github.com>
2022-07-14 13:56:07 +0100

    Added gallery

--- features/gallery/components/ImageCard.tsx
+++ features/gallery/components/ImageCard.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { Box, IconButton, Image } from "@chakra-ui/react";
+import { FaLink, FaTrash } from "react-icons/fa";
+import { Card } from "../../base/components/Card";
 
-const ImageCard = () => {
-  return <></>;
+type props = {
+  src: string;
+};
+
+const ImageCard = ({ src }: props) => {
+  return (
+    <Card position={"relative"} width={"fit-content"} role={"group"}>
+      <Image
+        src={src}
+        objectFit="cover"
+        maxH={{ base: "100%", md: "250px" }}
+        loading={"lazy"}
+      />
+      <Box
+        as={"section"}
+        display={"none"}
+        position={"absolute"}
+        bottom={0}
+        bg={"white"}
+        _groupHover={{ display: "block" }}
+        roundedTop={4}
+        left={"50%"}
+        transform={"translateX(-50%)"}
+      >
+        <IconButton
+          variant="ghost"
+          colorScheme="brand.100"
+          aria-label="Copy share link"
+          icon={<FaLink />}
+        />
+        <IconButton
+          variant="ghost"
+          colorScheme="red"
+          aria-label="Delete media"
+          icon={<FaTrash />}
+        />
+      </Box>
+    </Card>
+  );
 };
 
 export default ImageCard;
--- pages/dashboard/gallery.tsx
+++ pages/dashboard/gallery.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import BaseAppPage from "../../features/dashboard/components/BaseAppPage";
-import { Button, Flex, Input, Spacer } from "@chakra-ui/react";
+import { Button, Flex, Input, SimpleGrid, Spacer } from "@chakra-ui/react";
+import ImageCard from "../../features/gallery/components/ImageCard";
 
 const Gallery = () => {
+  const mediaList = [
+    { id: 0, src: "https://via.placeholder.com/800.png" },
+    { id: 1, src: "https://via.placeholder.com/800.png" },
+    { id: 2, src: "https://via.placeholder.com/800.png" },
+    { id: 3, src: "https://via.placeholder.com/800.png" },
+    { id: 4, src: "https://via.placeholder.com/800.png" },
+    { id: 5, src: "https://via.placeholder.com/800.png" },
+    { id: 6, src: "https://via.placeholder.com/800.png" },
+    { id: 7, src: "https://via.placeholder.com/800.png" },
+  ];
+
   return (
     <BaseAppPage title={"Gallery"}>
       <Flex p={5} bg={"white"} gap={5} boxShadow={"inset 0px -1px 0px #F1F1F1"}>
@@ -15,6 +27,11 @@ const Gallery = () => {
           Sort
         </Button>
       </Flex>
+      <SimpleGrid p={5} spacing={10} minChildWidth={"200px"}>
+        {mediaList.map((media) => (
+          <ImageCard src={media.src} key={media.id} />
+        ))}
+      </SimpleGrid>
     </BaseAppPage>
   );
 };
