c74831edffc1b10c8ee704011e8e31a48f41e93d
Ryan-Oneil <44525616+Ryan-Oneil@users.noreply.github.com>
2022-07-24 09:06:48 +0100

    Update FirebaseAdmin.ts

--- features/Auth/FirebaseAdmin.ts
+++ features/Auth/FirebaseAdmin.ts
@@ -3,7 +3,7 @@ import * as firebaseAdmin from "firebase-admin";
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: JSON.parse(process.env.firebase_private_key || ""),
+      privateKey: JSON.parse(process.env.firebase_private_key as string),
       clientEmail: process.env.firebase_client_email,
       projectId: process.env.firebase_project_id,
     }),
