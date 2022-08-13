c3cdf6c8acfe0026e334f7ed7b52d57e2b8b9bf6
Ryan-Oneil <44525616+Ryan-Oneil@users.noreply.github.com>
2022-08-13 15:36:48 +0100

    Update files.tsx

--- client/pages/dashboard/files.tsx
+++ client/pages/dashboard/files.tsx
@@ -3,8 +3,13 @@ import BaseAppPage from "../../features/dashboard/components/BaseAppPage";
 import { Box, Heading, SimpleGrid } from "@chakra-ui/react";
 import FileCard from "../../features/fileshare/components/FileCard";
 import RecentFileUploads from "../../features/dashboard/components/RecentFileUploads";
+import { GetServerSidePropsContext } from "next";
+import { getUserFromRequest } from "../../features/Auth/FirebaseAdmin";
+import User from "../../lib/mongoose/model/User";
+import { SharedLink } from "../../features/dashboard/types/SharedFile";
+import dbConnect from "../../lib/mongoose";
 
-const Files = () => {
+const Files = ({ sharedLinks }: { sharedLinks: [SharedLink] }) => {
   return (
     <BaseAppPage title={"Shared Files"}>
       <Box as={"section"} p={5}>
@@ -27,10 +32,28 @@ const Files = () => {
           <FileCard />
           <FileCard />
         </SimpleGrid>
-        <RecentFileUploads />
+        <RecentFileUploads links={sharedLinks} />
       </Box>
     </BaseAppPage>
   );
 };
 
 export default Files;
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const uid = await getUserFromRequest(context);
+
+  await dbConnect();
+
+  const user = await User.findOne({ externalId: uid }, "sharedLinks")
+    .lean()
+    .exec();
+
+  return {
+    props: {
+      sharedLinks: JSON.parse(JSON.stringify(user.sharedLinks)),
+    },
+  };
+};
