ad44c991688e93f5f981b6144ab51835009b066a
Ryan-Oneil <44525616+Ryan-Oneil@users.noreply.github.com>
2022-09-20 18:21:22 +0100

    Update Inputs.js

--- src/features/base/components/forms/Inputs.js
+++ src/features/base/components/forms/Inputs.js
@@ -1,13 +1,11 @@
-import React, { useRef } from "react";
+import React from "react";
 import {
-  Button,
   FormControl,
   FormErrorMessage,
   FormLabel,
   Input,
   InputGroup,
   InputLeftElement,
-  Textarea,
 } from "@chakra-ui/react";
 
 export const LabeledInput = (props) => {
@@ -21,39 +19,6 @@ export const LabeledInput = (props) => {
         {icon && <InputLeftElement pointerEvents="none" children={icon} />}
         <Input {...props} />
       </InputGroup>
-      {error && touched && <FormErrorMessage>{error}</FormErrorMessage>}
-    </FormControl>
-  );
-};
-
-export const FileInput = (props) => {
-  const { error, touched, buttonText } = props;
-  const ref = useRef();
-
-  return (
-    <FormControl isInvalid={error && touched}>
-      <Button onClick={() => ref.current.click()}>{buttonText}</Button>
-      <Input
-        size="lg"
-        {...props}
-        type={"file"}
-        ref={ref}
-        display={"none"}
-        value={""}
-      />
-      {error && touched && <FormErrorMessage>{error}</FormErrorMessage>}
-    </FormControl>
-  );
-};
-
-export const TextAreaInput = (props) => {
-  const { error, touched, label } = props;
-  const isInvalid = error && touched;
-
-  return (
-    <FormControl isInvalid={isInvalid}>
-      <FormLabel>{label}</FormLabel>
-      <Textarea size="lg" {...props} isInvalid={isInvalid} />
       {error && touched && <FormErrorMessage>{error}</FormErrorMessage>}
     </FormControl>
   );
