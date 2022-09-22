07702030711d6ddcce6aacf30b2ccaa513c43ea6
Ryan-Oneil <44525616+Ryan-Oneil@users.noreply.github.com>
2022-09-22 18:07:02 +0100

    Added title support

--- src/features/Auth/components/BaseAuthPage.tsx
+++ src/features/Auth/components/BaseAuthPage.tsx
@@ -5,6 +5,7 @@ import { DASHBOARD_URL, HOMEPAGE_URL } from "@/utils/urls";
 import { useAuth } from "../hooks/useAuth";
 import { useRouter } from "next/router";
 import Image from "next/image";
+import Head from "next/head";
 
 type props = {
   title: string;
@@ -31,53 +32,58 @@ const BaseAuthPage = ({
   }, [auth]);
 
   return (
-    <Box
-      background={
-        isMobileDevice
-          ? backgroundColor
-          : `linear-gradient(to left, #1A202C 50%, ${backgroundColor} 50%);`
-      }
-      h={"100vh"}
-      overflow={"auto"}
-    >
-      <NextLink href={HOMEPAGE_URL}>
-        <Heading
-          size={"lg"}
-          fontWeight="bold"
-          p={30}
-          w={"fit-content"}
-          cursor={"pointer"}
-        >
-          Media Share
-        </Heading>
-      </NextLink>
+    <>
+      <Head>
+        <title>{title} - Media Share</title>
+      </Head>
+      <Box
+        background={
+          isMobileDevice
+            ? backgroundColor
+            : `linear-gradient(to left, #1A202C 50%, ${backgroundColor} 50%);`
+        }
+        h={"100vh"}
+        overflow={"auto"}
+      >
+        <NextLink href={HOMEPAGE_URL}>
+          <Heading
+            size={"lg"}
+            fontWeight="bold"
+            p={30}
+            w={"fit-content"}
+            cursor={"pointer"}
+          >
+            Media Share
+          </Heading>
+        </NextLink>
 
-      {!isMobileDevice && (
-        <HStack align={"start"}>
-          <Box w={"50vw"}>{children}</Box>
-          <Box w={"50vw"} as={"section"} color={"white"}>
-            <Box w={"70%"} m={"auto"}>
-              <Image
-                src={"/share.svg"}
-                alt={"Upload logo"}
-                width={600}
-                height={500}
-              />
-              <Heading
-                mt={{ base: 0, "2xl": "60px" }}
-                fontSize={{ base: "3xl", "2xl": "5xl" }}
-              >
-                {title}
-              </Heading>
-              <Text mt={5} fontSize={"xl"}>
-                Media Share
-              </Text>
+        {!isMobileDevice && (
+          <HStack align={"start"}>
+            <Box w={"50vw"}>{children}</Box>
+            <Box w={"50vw"} as={"section"} color={"white"}>
+              <Box w={"70%"} m={"auto"}>
+                <Image
+                  src={"/share.svg"}
+                  alt={"Upload logo"}
+                  width={600}
+                  height={500}
+                />
+                <Heading
+                  mt={{ base: 0, "2xl": "60px" }}
+                  fontSize={{ base: "3xl", "2xl": "5xl" }}
+                >
+                  {title}
+                </Heading>
+                <Text mt={5} fontSize={"xl"}>
+                  Media Share
+                </Text>
+              </Box>
             </Box>
-          </Box>
-        </HStack>
-      )}
-      {isMobileDevice && children}
-    </Box>
+          </HStack>
+        )}
+        {isMobileDevice && children}
+      </Box>
+    </>
   );
 };
 
--- src/features/dashboard/components/BaseAppPage.tsx
+++ src/features/dashboard/components/BaseAppPage.tsx
@@ -17,6 +17,7 @@ import { FaBars } from "react-icons/fa";
 import SettingDropDown from "./SettingDropDown";
 import NotificationButton from "./NotificationButton";
 import StorageStatus from "./StorageStatus";
+import Head from "next/head";
 
 interface BaseAppPageProps extends BoxProps {
   title: string;
@@ -35,52 +36,57 @@ const BaseAppPage = ({
   const [showDrawer, setShowDrawer] = useState(false);
 
   return (
-    <Flex flexWrap={{ base: "wrap", md: "nowrap" }} minH={"100vh"}>
-      <Sidebar display={{ base: "none", md: "block" }}>
-        <StorageStatus used={used} max={max} />
-      </Sidebar>
-      <Flex w={"100%"} bg={"#FAFAFA"} h={"100vh"} flexDirection={"column"}>
-        <Flex
-          alignItems={"center"}
-          as="nav"
-          bg={"white"}
-          boxShadow={"inset 0px -1px 0px #F1F1F1"}
-          p={3}
-        >
-          <IconButton
-            display={{ base: "inline-flex", md: "none" }}
-            icon={<FaBars />}
-            variant="outline"
-            onClick={() => setShowDrawer(true)}
-            aria-label={"Menu label"}
-            mr={5}
-          />
-          <Heading size={"md"}>{title}</Heading>
-          <Spacer />
-          <NotificationButton />
-          <SettingDropDown />
+    <>
+      <Head>
+        <title>{title} - Media Share</title>
+      </Head>
+      <Flex flexWrap={{ base: "wrap", md: "nowrap" }} minH={"100vh"}>
+        <Sidebar display={{ base: "none", md: "block" }}>
+          <StorageStatus used={used} max={max} />
+        </Sidebar>
+        <Flex w={"100%"} bg={"#FAFAFA"} h={"100vh"} flexDirection={"column"}>
+          <Flex
+            alignItems={"center"}
+            as="nav"
+            bg={"white"}
+            boxShadow={"inset 0px -1px 0px #F1F1F1"}
+            p={3}
+          >
+            <IconButton
+              display={{ base: "inline-flex", md: "none" }}
+              icon={<FaBars />}
+              variant="outline"
+              onClick={() => setShowDrawer(true)}
+              aria-label={"Menu label"}
+              mr={5}
+            />
+            <Heading size={"md"}>{title}</Heading>
+            <Spacer />
+            <NotificationButton />
+            <SettingDropDown />
+          </Flex>
+          <Box as={"article"} {...rest} overflow={"auto"} h={"100%"}>
+            {children}
+          </Box>
         </Flex>
-        <Box as={"article"} {...rest} overflow={"auto"} h={"100%"}>
-          {children}
-        </Box>
+        {showDrawer && (
+          <Drawer
+            isOpen={showDrawer}
+            placement="left"
+            onClose={() => setShowDrawer(false)}
+          >
+            <DrawerOverlay>
+              <DrawerContent>
+                <DrawerCloseButton color={"white"} />
+                <DrawerBody p={0}>
+                  <Sidebar />
+                </DrawerBody>
+              </DrawerContent>
+            </DrawerOverlay>
+          </Drawer>
+        )}
       </Flex>
-      {showDrawer && (
-        <Drawer
-          isOpen={showDrawer}
-          placement="left"
-          onClose={() => setShowDrawer(false)}
-        >
-          <DrawerOverlay>
-            <DrawerContent>
-              <DrawerCloseButton color={"white"} />
-              <DrawerBody p={0}>
-                <Sidebar />
-              </DrawerBody>
-            </DrawerContent>
-          </DrawerOverlay>
-        </Drawer>
-      )}
-    </Flex>
+    </>
   );
 };
 
